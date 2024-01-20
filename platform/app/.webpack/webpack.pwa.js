7ab55dc5c061011bc98b03bfb1b34c8613a85113
LH <34941093+fenyuzhe@users.noreply.github.com>
2024-01-20 09:58:41 +0800

    配置文件中增加clinicalviewer模式

--- platform/app/.webpack/webpack.pwa.js
+++ platform/app/.webpack/webpack.pwa.js
@@ -69,6 +69,7 @@ module.exports = (env, argv) => {
         // Hoisted Yarn Workspace Modules
         path.resolve(__dirname, '../../../node_modules'),
         SRC_DIR,
+        path.resolve(__dirname, 'modes/clinical-workstation-mode/node_modules'),
       ],
     },
     plugins: [
