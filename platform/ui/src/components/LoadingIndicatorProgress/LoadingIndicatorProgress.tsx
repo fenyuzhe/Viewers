c6d2125694f27eb8c8720a95aadedf8169ded4b8
LH <34941093+fenyuzhe@users.noreply.github.com>
2024-03-27 10:44:14 +0800

    修改loading（加载中）的样式，去掉了ohif的图标

--- platform/ui/src/components/LoadingIndicatorProgress/LoadingIndicatorProgress.tsx
+++ platform/ui/src/components/LoadingIndicatorProgress/LoadingIndicatorProgress.tsx
@@ -18,10 +18,11 @@ function LoadingIndicatorProgress({ className, textBlock, progress }) {
         className
       )}
     >
-      <Icon
-        name="loading-ohif-mark"
+      {/* <Icon
+        // name="loading-ohif-mark"
+        name="notifications-info"
         className="h-12 w-12 text-white"
-      />
+      /> */}
       <div className="w-48">
         <ProgressLoadingBar progress={progress} />
       </div>
